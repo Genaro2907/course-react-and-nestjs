00acec23c568b2a16871282fab01025204166a18
Gabriel Genaro Cardoso Ferreira <g.genaro.contato@gmail.com>
2026-05-25 17:36:01 -0300

    Botão e Rodapé

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -6,6 +6,9 @@ import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 export function App() {
     return (
@@ -36,10 +39,14 @@ export function App() {
                         <Cycles />
                     </div>
                     <div className='formRow'>
-                        <button>Enviar</button>
+                        <DefaultButton icon={<PlayCircleIcon />} color='green' />
                     </div>
+
                 </form>
             </Container>
+            <Container>
+                <Footer />
+            </Container>
         </>
-    );
+    )
 }
